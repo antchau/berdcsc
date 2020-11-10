d88e2d74e6b6980301c621f0fb04307ac6b460db
Anthony Chau <achau713@gmail.com>
2020-11-10 12:04:44 -0800

    Add color scales functions

---
+++ R/color_scales.R
@@ -0,0 +1,48 @@
+#' Color scale constructor for a color palette
+#'
+#' @param palette_name Character name of color palette
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param discrete Boolean indicating whether color aesthetic is discrete or not
+#' @param n Integer indicating the number of colors to return after interpolating the given colors in the palette
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+scale_color_palette <- function(palette_name = "main", reverse = FALSE, discrete = TRUE, n, ...) {
+
+  palette <- interpolate_color(palette = palette_name, reverse = reverse, n = n)
+
+  if (discrete) {
+    discrete_scale("colour",
+                   scale_name = paste0(palette_name, "palette"),
+                   palette = palette,
+                   ...)
+  } else {
+    scale_color_gradientn(colours = palette, ...)
+  }
+}
+
+#' Fill scale constructor for a color palette
+#'
+#' @param palette_name Character name of color palette s
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param discrete Boolean indicating whether color aesthetic is discrete or not
+#' @param n Integer indicating the number of colors to return after interpolating the given colors in the palette
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+scale_fill_palette <- function(palette_name = "main", reverse = FALSE, discrete = TRUE, n = n, ...) {
+
+  palette <- interpolate_color(palette = palette_name, reverse = reverse, n = n)
+
+  if (discrete) {
+    discrete_scale("fill",
+                   scale_name = paste0(palette_name, "palette"),
+                   palette = pal,
+                   ...)
+  } else {
+    scale_fill_gradientn(colours = palette, ...)
+  }
+}
+
+
+
---
+++ tests/testthat/test-color_scales.R
@@ -0,0 +1,12 @@
+context("Color scales")
+library(berdcsc)
+
+
+# test_that("scale_colour_palette returns error if number of colors doesn't equal number of discrete categories", {
+#   expect_equal()
+# })
+#
+#
+# test_that("scale_fill_palette returns error if number of colors doesn't equal number of discrete categories", {
+#   expect_equal()
+# })
