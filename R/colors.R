9d255d8469463ba97d2a7b2f5ff85397d1aee7b2
Anthony Chau <achau713@gmail.com>
2020-11-10 02:44:37 -0800

    Add color interpolation function

--- R/colors.R
+++ R/colors.R
@@ -1,6 +1,6 @@
 #' Return UCI colors
 #'
-#' @return A named vector of UCI color names with corresponding hex code
+#' @return A named vector of UCI color names with corresponding hex codes
 #' @export
 #'
 #' @examples
@@ -10,8 +10,8 @@ uci_colors <- function(){
   colors <- c(
   "uci-blue" = "#0064A4",
   "uci-gold" = "#FFD200",
-  "light-blue" = "6AA2B8",
-  "light-gray" = "C6BEB5",
+  "light-blue" = "#6AA2B8",
+  "light-gray" = "#C6BEB5",
   "dark-blue" = "#1B3D6D",
   "orange" = "#F78D2D",
   "light-yellow" = "#F7EB5F",
@@ -43,3 +43,64 @@ get_hex_color <- function(...){
 
   return(uci_colors()[color_names])
 }
+
+
+#' Create color palettes
+#'
+#' @param ... Character names of colors
+#'
+#' @return A list of named vectors. Each vector is a color palette with each element containing the
+#' color name and corresponding hex code
+#' @export
+#'
+#' @examples
+#' create_color_palette()
+#' create_color_palette(...)
+get_color_palette <- function(...){
+
+  palette <- c(...)
+
+  berdcsc_palettes <- list(
+    main = get_hex_color("uci-blue", "uci-gold", "dark-gray"),
+    light = get_hex_color("light-blue", "light-yellow", "light-gray"),
+    dark = get_hex_color("dark-blue", "orange", "dark-gray")
+  )
+
+  if(is.null(palette)){
+    return(berdcsc_palettes)
+  }
+
+  return(palette)
+
+}
+
+
+
+#' Interpolate colors
+#'
+#' @param palette Character name of the color palette
+#' @param reverse Boolean indicating whether to reverse palette
+#' @param n Integer indicating the number of colors to return after interpolating the given colors in the palette
+#' @param ... Additional arguments to colorRampPalette()
+#'
+#' @return A character vector of length n that contains colors interpolated from the palette
+#' @export
+#'
+#' @examples
+#' interpolate_color(get_color_palette())
+interpolate_color <- function(palette = "main", reverse = FALSE, n , ...){
+
+  if(!hasArg(n)){
+    stop("Please supply a value for n, the number of colors to return after interpolation")
+  }
+
+  palette <- get_color_palette()[[palette]]
+
+  if(reverse) {
+    palette <- rev(palette)
+  }
+
+  return(grDevices::colorRampPalette(palette, ...)(n))
+
+}
+
--- tests/testthat/test-colors.R
+++ tests/testthat/test-colors.R
@@ -5,3 +5,22 @@ test_that("get_hex_color returns a hex code given a UCI color", {
   expect_named(get_hex_color("uci-blue"), c("uci-blue"))
   expect_named(get_hex_color("uci-gold", "light-blue"), c("uci-gold", "light-blue"))
 })
+
+test_that("get_color_palette returns a color palette", {
+  expect_named(get_color_palette()[["main"]], c("uci-blue", "uci-gold", "dark-gray"))
+  expect_named(get_color_palette()[["light"]], c("light-blue", "light-yellow", "light-gray"))
+  expect_named(get_color_palette()[["dark"]], c("dark-blue", "orange", "dark-gray"))
+})
+
+
+test_that("interpolate_color returns the correct number of colors after interpolation", {
+  expect_length(interpolate_color(n = 10), 10)
+  expect_length(interpolate_color(palette = "light", n = 3), 3)
+})
+
+
+test_that("interpolate_color returns an error if n is not supplied", {
+  expect_error(interpolate_color(), "Please supply a value for n, the number of colors to return after interpolation")
+})
+
+
